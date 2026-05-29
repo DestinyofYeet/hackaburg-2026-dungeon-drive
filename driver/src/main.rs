fa4768ea5a539d0e5270d2bbc2d4e3eb4adbbd79
DestinyofYeet <ole@ole.blue>
2026-05-29 21:26:25 +0200

    feat(driver): test

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -51,7 +51,7 @@ fn main() {
 
     {
         println!("Moving");
-        let mut motor_x = Motor::new(5, 6);
+        let mut motor_x = Motor::new(17, 27);
         motor_x.step_motor(StepMotor::Forward { steps: 20 });
         let mut motor_y = Motor::new(23, 24);
     }
