26348ff0467df590e49fd0d3e9ab3e77a7d00c92
DestinyofYeet <ole@ole.blue>
2026-05-29 22:00:44 +0200

    chore(driver): increase steps

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -52,7 +52,7 @@ fn main() {
     {
         println!("Moving");
         let mut motor_x = Motor::new(17, 27);
-        motor_x.step_motor(StepMotor::Forward { steps: 20 });
+        motor_x.step_motor(StepMotor::Forward { steps: 200 });
         let mut motor_y = Motor::new(23, 24);
     }
 
